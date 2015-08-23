a30428ab1fa96dde4483c74a55397cacfe6d0f1a
Kai Trott <k.trott@web.de>
2015-08-24 01:47:31 +0200

    SimpleMenu Handler
    - Added Lifebar
    - set specific handler for every Menu Item
    - removed Aplite Support (for now)

--- src/PkFight.c
+++ src/PkFight.c
@@ -5,19 +5,44 @@ static TextLayer *text_layer;
 static SimpleMenuLayer * s_simple_menu_layer;
 static SimpleMenuItem s_menu_items[4];
 static SimpleMenuSection s_menu_sections[1];
-static char *s_menu_items_names[4] = {"Attack", "Bag", "Monster",  "Run"};
 
 void setup_menu_items();
 void setup_menu_sections();
 
+static void menu_handler_attack (int index, void *context) {
+    text_layer_set_text(text_layer, "Lets Attack!");
+}
+
+static void menu_handler_bag (int index, void *context) {
+    text_layer_set_text(text_layer, "Lets look in our bag!");
+}
+
+static void menu_handler_monster (int index, void *context) {
+    text_layer_set_text(text_layer, "Lets change the Monster!");
+}
+
+static void menu_handler_run (int index, void *context) {
+    text_layer_set_text(text_layer, "Lets RUN AWAY!!!");
+}
+
 void setup_menu_items()
 {
-  for (int i = 0; i < (int)(sizeof(s_menu_items_names) / sizeof(s_menu_items_names[0])); i++)
-  {
-    s_menu_items[i] = (SimpleMenuItem){
-      .title = s_menu_items_names[i],
-    };
-  }
+  s_menu_items[0] = (SimpleMenuItem){
+    .title = "Attack",
+    .callback = menu_handler_attack,
+  };
+  s_menu_items[1] = (SimpleMenuItem){
+    .title = "Bag",
+    .callback = menu_handler_bag,
+  };
+  s_menu_items[2] = (SimpleMenuItem){
+    .title = "Monster",
+    .callback = menu_handler_monster,
+  };
+  s_menu_items[3] = (SimpleMenuItem){
+    .title = "RUN",
+    .callback = menu_handler_run,
+  };
 }
 
 void setup_menu_sections()
@@ -25,20 +50,9 @@ void setup_menu_sections()
   s_menu_sections[0] = (SimpleMenuSection)
   {
     .title = NULL,
-      .items = s_menu_items,
-      .num_items = sizeof(s_menu_items) / sizeof(s_menu_items[0])
+    .items = s_menu_items,
+    .num_items = sizeof(s_menu_items) / sizeof(s_menu_items[0]),
   };
-}
-
-static void menu_handler(int index, void *context) {
-  switch (index)
-  {
-    case 0: text_layer_set_text(text_layer, "0"); break;
-    case 1: text_layer_set_text(text_layer, "1"); break;
-    case 2: text_layer_set_text(text_layer, "2"); break;
-    case 3: text_layer_set_text(text_layer, "3"); break;
-    case 4: text_layer_set_text(text_layer, "4"); break;
-  }
 }
 
 static void click_config_provider(void *context) {
@@ -47,17 +61,24 @@ static void click_config_provider(void *context) {
   //window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
 }
 
+static void update_proc(Layer *layer, GContext *ctx) {
+  graphics_context_set_fill_color(ctx, GColorFromRGB(0, 255, 0));
+  graphics_fill_rect(ctx, GRect(90, 105, 30, 3), 0, GCornerNone);
+  graphics_fill_rect(ctx, GRect(10, 15, 30, 3), 0, GCornerNone);
+}
+
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  s_simple_menu_layer = simple_menu_layer_create((GRect) { .origin = { 0, 105 }, .size = { bounds.size.w, 45 }}, window, s_menu_sections, 1, menu_handler);
+  s_simple_menu_layer = simple_menu_layer_create((GRect) { .origin = { 0, 125 }, .size = { bounds.size.w, 45 }}, window, s_menu_sections, 1, NULL);
   layer_add_child(window_layer, (Layer *)s_simple_menu_layer);
 
   text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
   text_layer_set_text(text_layer, "ExampleText");
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
+  layer_set_update_proc(window_layer, update_proc);
 }
 
 static void window_unload(Window *window) {
@@ -69,7 +90,7 @@ static void init(void) {
 
   setup_menu_items();
   setup_menu_sections();
-  //window_set_background_color(window, GColorRed);
+  window_set_background_color(window, GColorWhite);
 
   window_set_click_config_provider(window, click_config_provider);
   window_set_window_handlers(window, (WindowHandlers) {
